fcfd1d2269693364bab53a6fdbe42b87e5cc0fa4
brianweber338 <brianweber338@gmail.com>
2022-01-28 12:12:16 -0500

    Added 3 Vector Math Operations

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,10 @@
 print("Hello World")
+vector1 <- c(182, 182.1, 182.01, 182.001)
+vector1/5
+
+vector2 <- c(100, 120, 140, 180)
+vector1 + vector2
+
+vector3 <- c(4, 8, 16, 32)
+vectorFinal <- vector2/vector3
+print(vectorFinal)
