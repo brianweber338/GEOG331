6d9a1f0f96d82d3f6a2a7ff2f636aa2f45f2f5b2
brianweber338 <brianweber338@gmail.com>
2022-02-02 12:11:33 -0500

    Almost done with question 2

--- Activity2.R
+++ Activity2.R
@@ -8,14 +8,21 @@ print(heights_cm)
 # 99:1
 # help(matrix)
 # ?matrix
+# rm(ElementIWantRemoved)
+
 
 #set up a matrix with 2 columns and fill in by rows
 #first argument is the vector of numbers to fill in the matrix
 Mat<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
 
 
+#HW START
+
+setwd("Z:\\students\\bweber\\Data\\Activity2Files")
+getwd()
+
 # In class PC File Path
-datW <- read.csv("C:\\Users\\bweber\\Documents\\Activity2Files\\2011124.csv", stringsAsFactors = T)
+datW <- read.csv("Z:\\students\\bweber\\Data\\Activity2Files\\2011124.csv", stringsAsFactors = T)
 str(datW)
 
 #Question 1
@@ -32,13 +39,13 @@ datW$year <- as.numeric(format(datW$dateF,"%Y"))
 
 #Question 2
 # Character Data
-
+CharVector <- c("My",  "GitHub", "Username", "Is", "brianweber338")
 
 # Numeric Data
-
+NumericVector <- c(-3.75, -2.5, -1.25, 0, 1.25, 2.5, 3.75)
 
 # Integer Data
-
+IntVector <- c(-1L, 2L, 4L, 8L, 16L)
 
 # Factor Data
 
