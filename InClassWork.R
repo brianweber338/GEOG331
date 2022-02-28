990e9e598142ea6dd53995aeb833c72fe6a26f79
brianweber338 <brianweber338@gmail.com>
2022-02-28 11:50:59 -0500

    Updated in class work

--- InClassWork.R
+++ InClassWork.R
@@ -1,13 +1,44 @@
-flower <- iris[iris$Species=="virginica"]
+# Subset for Iris Virginica
+flower <- iris[iris$Species=="virginica",]
 
-#lm(y ~ x)
-fit <- l(flower$Petal.length ~ flower$Sepal.length)
+# lm(y ~ x)
+# Linear model relating to petal length to sepal length
+fit <- lm(flower$Petal.Length ~ flower$Sepal.Length)
 
+# View Results
 summary(fit)
 
+
+# Create a scatter plot
+plot(flower$Sepal.Length,flower$Petal.Length,
+     main = "Iris virginica",
+     xlab = "Sepal Length",
+     ylab = "Petal Length",
+     col = "purple", pch = 16)
+
+# plot the residuals, stored in regression summary
 plot(flower$Sepal.Length, summary(fit)$residuals,
-     xlab = "Sepal length", ylab = "residuals", )
+     xlab = "Sepal length",
+     ylab = "residuals", 
+     col = "purple",
+     pch = 16)
 
-hist(summary(fit)$residuals)
+# add a horizontal line ro reference
+abline(h=0,
+       lty = "dashed")
 
+# histogram of residuals
+hist(summary(fit)$residuals,
+     main = "Regression Residuals",
+     xlab = "Residual",
+     col = "purple")
+
+# shapiro wilks test
 shapiro.test(summary(fit)$residuals)
+
+# qq plot
+qqnorm(summary(fit)$residuals, pch = 16)
+
+qqline(summary(fit)$residuals, datax = FALSE, distribution = qnorm,
+       probs = c(0.25, 0.75), qtype = 7, pch = 16)
+
