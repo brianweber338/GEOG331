adf4bef1fc87ea4105e1538326bd3125c46c7730
brianweber338 <brianweber338@gmail.com>
2022-03-28 12:18:11 -0400

    Up to Question 5 Complete

--- Activity5.R
+++ Activity5.R
@@ -2,13 +2,13 @@
 library(lubridate)
 
 #read in streamflow data
-datH <- read.csv("data/stream_flow_data.csv",
+datH <- read.csv("Z:\\data\\streamflow\\stream_flow_data.csv",
                  na.strings = c("Eqp"))
 head(datH)
 
 #read in precipitation data
 #hourly precipitation is in mm
-datP <- read.csv("data/2049867.csv")                            
+datP <- read.csv("Z:\\data\\streamflow\\2049867.csv")                            
 head(datP)
 
 #only use most reliable measurements
@@ -47,3 +47,55 @@ datP$decDay <- datP$doy + (datP$hour/24)
 #calculate a decimal year, but account for leap year
 datP$decYear <- ifelse(leap_year(datP$year),datP$year + (datP$decDay/366),
                        datP$year + (datP$decDay/365))          
+
+
+
+
+#### Question 3 ####
+#plot discharge
+dev.new(width=8,height=8)
+plot(datD$decYear, datD$discharge, type="l", xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+
+#### Question 4 ####
+
+
+#### Question 5 ####
+#basic formatting
+aveF <- aggregate(datD$discharge, by=list(datD$doy), FUN="mean")
+colnames(aveF) <- c("doy","dailyAve")
+sdF <- aggregate(datD$discharge, by=list(datD$doy), FUN="sd")
+colnames(sdF) <- c("doy","dailySD")
+
+#start new plot
+dev.new(width=8,height=8)
+#bigger margins
+par(mai=c(1,1,1,1))
+#make plot
+plot(aveF$doy,aveF$dailyAve, 
+     type="l", 
+     xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2,
+     ylim=c(0,90),
+     xaxs="i", yaxs ="i",#remove gaps from axes
+     axes=FALSE)#no axes
+polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
+        c(aveF$dailyAve-sdF$dailySD,rev(aveF$dailyAve+sdF$dailySD)),#ycoord
+        col=rgb(0.392, 0.584, 0.929,.2), #color that is semi-transparent
+        border=NA#no border
+)
+# Adds 2015 line
+lines(datD$discharge[datD$year == "2017"],
+      col= rgb(95/255,158/255,160/255,.5), pch=15)
+
+axis(1, seq(15,345, by=30), #tick intervals
+     lab=seq(15,345, by=30)) #tick labels
+axis(2, seq(0,80, by=20),
+     seq(0,80, by=20),
+     las = 2)#show ticks at 90 degree angle
+legend("topright", c("mean","1 standard deviation"), #legend items
+       lwd=c(2,NA),#lines
+       col=c("black",rgb(0.392, 0.584, 0.929,.2)),#colors
+       pch=c(NA,15),#symbols
+       bty="n")#no legend border
