6a064b02e2c65b49b7d8a94f0cbf6f8e687cf103
brianweber338 <brianweber338@gmail.com>
2022-04-13 12:01:37 -0400

    Added to file on 4/13

--- Terra_In_Class_Work_April_6.R
+++ Terra_In_Class_Work_April_6.R
@@ -19,3 +19,37 @@ plotRGB(p, r = 3, g = 2, b = 1)
 plotRGB(p, r = 3, g = 2, b = 1, 
         scale = 65535, 
         stretch = "hist")
+
+
+# read file with field observations of canopy cover
+tree <- read.csv("Z:/data/rs_data/siberia_stand_data.csv",
+                 header = T)
+
+# convert to vector object using terra package
+gtree <- vect(tree, geom = c("Long", "Lat"), "epsg:4326")
+
+# project the data to match the coordinate system of the raster layer
+gtree2 <- project(gtree, p)
+
+# create a polygon from the extent of the points
+# note - I would expect the crs to be carried over, but it isn't, so we must specify it again
+b <- as.lines(ext(gtree), "epsg:4326")
+
+# reproject the polygons to the same projection as our raster
+# crs: coordinate reference system
+b2 <- project(b,crs(p))
+
+# buffer the extent by 200m
+b3 <- buffer(b2, width= 200)
+
+# add gtree2 to plot
+plot(gtree2, add = T, col = "red")
+# add b2 square to plot
+plot(b2, add = T)
+# add b3 square to plot
+plot(b3, add = T)
+
+
+# use this to crop the raster layer so we
+# p2 <- crop(p, b3,
+#           filename = "")
